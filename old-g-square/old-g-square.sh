7ee1642bdf5132ca975dd003370fe7e58ffafeab
hammerhead-n5 <63978633+hammerhead-n5@users.noreply.github.com>
2020-04-24 12:50:02 +0100

    Update old-g-square.sh

--- old-g-square/old-g-square.sh
+++ old-g-square/old-g-square.sh
@@ -3,5 +3,5 @@ adb reboot bootloader
 tmp_dir=$(mktemp -d -t XXXXXXXXXX)
 cd $tmp_dir
 wget https://github.com/hammerhead-n5/bootgraphics/raw/master/old-g-square/old-g-square.img
-fastboot flash imgdata ./old-g-sqaure.img
+fastboot flash imgdata ./old-g-square.img
 fastboot reboot
